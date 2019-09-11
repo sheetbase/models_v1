b3ee7dd039e51995cd875d14f2fed4cfab98dcfd
Lam Nhan <lamnhan92@gmail.com>
2019-09-11 20:21:07 +0700

    fix #48

--- src/thread.ts
+++ src/thread.ts
@@ -1,11 +1,21 @@
 import { Ids, Status, Rating, Sharing, Statistics, Extras } from './_shared';
 
+export interface ThreadAttachment {
+  name: string;
+  mimeType: string;
+  size: number;
+  id?: string; // not exists for Gmail messages
+}
+
 export interface Thread extends Ids, Status, Rating, Sharing, Statistics, Extras {
   // Ids
   type?: string;
   // Status
   createdAt?: string;
   content: string;
+  attachments?: {
+    [index: string]: ThreadAttachment;
+  };
   parent?: string;
   master?: string; // <sheet>/<key> (posts/post-1)
   stars?: number; // with rating stars
